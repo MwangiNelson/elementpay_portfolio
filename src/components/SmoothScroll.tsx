bef829f404e37f4171d69ad63d9916d3e0f1566d
MwangiNelson <nelsonmwangi197@gmail.com>
2025-09-28 19:19:30 +0300

    [UPDATE]Lenis scroll update 2

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -8,10 +8,9 @@ export default function SmoothScroll() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      gestureDirection: "vertical",
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
+      
+   
+ 
       touchMultiplier: 2,
       infinite: false,
     });
