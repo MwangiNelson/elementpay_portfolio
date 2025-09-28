df0a6cfce067a5ca24751787ae556ce281747831
MwangiNelson <nelsonmwangi197@gmail.com>
2025-09-28 19:16:04 +0300

    [PATCH]Lenis scroll param update

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,15 +1,14 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
-import Lenis from 'lenis';
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 export default function SmoothScroll() {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
+      gestureDirection: "vertical",
       smooth: true,
       mouseMultiplier: 1,
       smoothTouch: false,
